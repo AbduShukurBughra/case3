915a417ea06bad57b24edaaa900391ae898136d2
“[AbduShukur]” <[kurshadbughra@gmail.com]>
2022-09-22 02:04:16 +0300

    Product controller completely

--- src/main/java/com/springbootproject/case3/controller/ProductController.java
+++ src/main/java/com/springbootproject/case3/controller/ProductController.java
@@ -9,33 +9,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     @Autowired
     private ProductEntityService productEntityService;
 
 //  •	Ürün kaydeden bir servis yazınız.
-    @PostMapping("/products")
+    @PostMapping("/product")
     public void saveProduct(@RequestBody Product product){
         productEntityService.save(product);
     }
 //  •	Ürünleri listeleyebilen bir servis yazınız.
 
 //  •	Ürün idsinden ürünü bulan bir servis yazınız.
-    @GetMapping("/products/{id}")
+    @GetMapping("/product/{id}")
     public Optional getProductById(Long id){
         Optional product = productEntityService.getProductById(id);
         return product;
     }
 
 //  •	Ürün silen bir metot yazınız.
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/product/{id}")
     public String deleteProductById(Long id){
         productEntityService.deleteProductById(id);
         return "Product is deleted...";
     }
 //  •	Ürün fiyatı güncelleyen bir metot yazınız. (Sadece fiyat)
-    @PutMapping("/products/{id}")
+    @PutMapping("/product/{id}")
     public String updateProductPrice(@PathVariable Long id, @RequestBody ProductDto productDto){
         productEntityService.updateProductPrice(id, productDto );
         return "Product price is updated successfully";
